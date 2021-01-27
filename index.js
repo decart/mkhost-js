47925707894dbb194dc624916793980d1a7f3353
mansur <yaponomat@gmail.com>
2021-01-27 10:02:38 +0300

    add ssl configuration

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const temp = require('./lib/template');
 
 const conf = new Configstore(packageJson.name, defaultsJson);
 const baseDir = conf.get('baseDir');
+const defaultCert = conf.get('sslCert');
+const defaultCertKey = conf.get('sslCertKey');
 const dirs = files.dirList(baseDir);
 
 /**
@@ -24,13 +26,27 @@ const main = async () => {
   const domainName = await inquirer.askDomainName();
   const sitesDirs = sug.fusesort(dirs, domainName);
   const rootDir = await inquirer.askRootDirectory(sitesDirs, baseDir);
+  const ssl = await inquirer.askSSL(defaultCert, defaultCertKey);
 
-  const vhost = temp.getView('http', {
+  let vhost = temp.getView('http', {
     host: domainName,
     root_dir: rootDir,
     index: conf.get('index'),
     fastcgi_pass: conf.get('fastcgiPass')
   });
+
+  if (ssl.need_ssl === true) {
+    const sslView = temp.getView('ssl', {
+      host: domainName,
+      root_dir: rootDir,
+      index: conf.get('index'),
+      fastcgi_pass: conf.get('fastcgiPass'),
+      cert: ssl.cert,
+      cert_key: ssl.cert_key,
+    });
+
+    vhost += '\n\n' + sslView;
+  }
 
   files.write(vhost, conf.get('nginxVHosts'), domainName + '.conf');
   files.append('\n127.0.0.1 ' + domainName, '/etc/hosts');
